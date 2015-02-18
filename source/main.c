2da03f00410a622e94a483b3778fdb771f0bee60
Andrii Sevastianov <mustitz@gmail.com>
2015-02-18 11:18:45 +0100

    Preparing for parsing, common scheme:
    Changes to be committed:
    	modified:   source/main.c

--- source/main.c
+++ source/main.c
@@ -1,6 +1,42 @@
 #include "checkers.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+struct line_parser
+{
+};
+
+int process_cmd(struct line_parser * restrict me, const char * cmd)
+{
+    return 1;
+}
+
 int main()
 {
+    srand(time(0));
+
+    char * line = 0;
+    size_t len = 0;
+
+    struct line_parser parser;
+
+    for (;; ) {
+        ssize_t hasRead = getline(&line, &len, stdin);
+        if (hasRead == -1) {
+            break;
+        }
+
+        int isQuit = process_cmd(&parser, line);
+        if (isQuit) {
+            break;
+        }
+    }
+
+    if (line) {
+        free(line);
+    }
+
     return 0;
 }
