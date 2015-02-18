8638e538d52af836bec1259ffafd0a9f0e1306b6
Andrii Sevastianov <mustitz@gmail.com>
2015-02-18 15:08:24 +0100

    Add handling of quit command (just exit).
    Changes to be committed:
    	modified:   source/main.c

--- source/main.c
+++ source/main.c
@@ -1,16 +1,89 @@
 #include "checkers.h"
+#include "mu-parser.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
-struct line_parser
+#define KW_QUIT          1
+
+#define ITEM(name) { #name, KW_##name }
+struct keyword_desc root_level_keywords[] = {
+    ITEM(QUIT),
+    { NULL, 0 }
+};
+#undef ITEM
+
+struct cmd_parser
 {
+    struct line_parser line_parser;
+    struct mempool * pool;
+    struct keyword_tracker * root_level;
 };
 
-int process_cmd(struct line_parser * restrict me, const char * cmd)
+static void init_parser(struct cmd_parser * restrict me, struct mempool * pool)
 {
+    me->pool = pool;
+
+    me->root_level = build_keyword_tracker(root_level_keywords, pool);
+    if (me->root_level == NULL) {
+        panic("Can not create root_level keyword tracker.");
+    }
+}
+
+static void error(struct cmd_parser * restrict me, const char * fmt, ...) __attribute__ ((format (printf, 2, 3))); 
+
+static void error(struct cmd_parser * restrict me, const char * fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    printf("Parsing error: ");
+    vprintf(fmt, args);
+    printf("\n");
+    va_end(args);
+}
+
+
+
+int process_quit(struct cmd_parser * restrict me)
+{
+    struct line_parser * restrict lp = &me->line_parser;
+    if (!parser_check_eol(lp)) {
+        error(me, "End of line expected (section declaration is over), but someting was found.");
+        return 0;
+    }
+
     return 1;
+}
+
+int process_cmd(struct cmd_parser * restrict me, const char * cmd)
+{
+    struct line_parser * restrict lp = &me->line_parser;
+    parser_set_line(lp, cmd);
+
+    if (parser_check_eol(lp)) {
+        return 0;
+    }
+
+    int keyword = parser_read_keyword(lp, me->root_level);
+
+    if (keyword == -1) {
+        error(me, "Invalid lexem at the begginning of the line.");
+        return 0;
+    }
+
+    if (keyword == 0) {
+        error(me, "Invalid keyword at the begginning of the line.");
+        return 0;
+    }
+
+    switch (keyword) {
+        case KW_QUIT:
+            return process_quit(me);  
+    }
+
+    error(me, "[INTERNAL] Unexpected keyword (%d) code was returned by parser_read_keyword.", keyword);
+    return 0;
 }
 
 int main()
@@ -20,7 +93,14 @@ int main()
     char * line = 0;
     size_t len = 0;
 
-    struct line_parser parser;
+    struct mempool * restrict pool = create_mempool(16000);
+    if (pool == NULL) {
+        panic("Can not create_mempool(16000)\n");
+        return 1;
+    }
+
+    struct cmd_parser parser;
+    init_parser(&parser, pool);
 
     for (;; ) {
         ssize_t hasRead = getline(&line, &len, stdin);
@@ -33,6 +113,8 @@ int main()
             break;
         }
     }
+
+    free_mempool(pool);
 
     if (line) {
         free(line);
