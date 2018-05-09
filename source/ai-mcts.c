3d3fdd6256ed1524d7338f2e681b5f53cf7d8233
Andrii Sevastianov <mustitz@gmail.com>
2018-05-09 20:45:23 +0300

    Add simple AI move logic.
    Make one rollout for each move and play one of the best.
    Now “rollout” function is a stub whick return zero (draw ½) every time.

--- source/ai-mcts.c
+++ source/ai-mcts.c
@@ -1,6 +1,7 @@
 #include "checkers.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct mcts_ai
 {
@@ -13,14 +14,59 @@ struct mcts_ai * get_mcts_ai(struct ai * const me)
     return move_ptr(me, -offsetof(struct mcts_ai, base));
 }
 
+static int rollout(struct mcts_ai * restrict const me, const struct position * const position)
+{
+    /* TODO: Not implemented */
+    return 0;
+}
+
+static int do_move(struct mcts_ai * restrict const me, struct move_ctx * restrict const move_ctx)
+{
+    const struct position * const answers = move_ctx->answers;
+    const int answer_count = move_ctx->answer_count;
+    if (answer_count == 1) {
+        return 0;
+    }
+
+    int best[answer_count];
+    int best_result = -1;
+    int qbest = 0;
+
+    for (int i=0; i<answer_count; ++i) {
+        const int result = -rollout(me, answers + i);
+        if (result < best_result) {
+            continue;
+        }
+        if (result == best_result) {
+            best[qbest++] = i;;
+            continue;
+        }
+
+        qbest = 1;
+        best[0] = i;
+        best_result = result;
+    }
+
+    if (qbest == 0) {
+        printf("Internal error: qbest == 0.\n");
+        return -1;
+    }
+
+    const int num = rand() % qbest;
+    return best[num];
+}
+
+
+
+/* API */
+
 void mcts_ai_set_position(struct ai * restrict me, const struct position * position)
 {
 }
 
 int  mcts_ai_do_move(struct ai * restrict me, struct move_ctx * restrict move_ctx)
 {
-    /* TODO: Here is a stub */
-    return 0;
+    return do_move(get_mcts_ai(me), move_ctx);
 }
 
 void mcts_ai_free(struct ai * restrict ai)
