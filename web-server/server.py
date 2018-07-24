ab9d19c5bc3696f95591bd3a60210f06b3201a37
Andrii Sevastianov <mustitz@gmail.com>
2018-07-24 18:08:25 +0300

    Sid generated and returned in index.html.

--- web-server/server.py
+++ web-server/server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 
+import hashlib
 import json
+import re
 
 from email.utils import formatdate
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -10,6 +12,8 @@ from os.path import abspath
 from socketserver import ThreadingMixIn
 from shutil import copyfileobj
 from stat import S_ISREG
+from threading import Lock
+from time import time
 
 
 class Cfg:
@@ -34,6 +38,35 @@ def safe_cast(val, to_type, default=None):
         return default
 
 
+class SessionManager:
+    @classmethod
+    def init(cls):
+        cls.lock = Lock()
+        cls.sessions = {}
+        pass
+
+    @classmethod
+    def get(cls, sid):
+        cls.lock.acquire()
+        try:
+            obj = cls.sessions.get(sid)
+            if type(obj) == dict:
+                obj['last_access'] = time()
+                cls.sessions[sid] = obj
+            return obj
+        finally:
+            cls.lock.release()
+
+    @classmethod
+    def set(cls, sid, obj):
+        obj['last_access'] = time()
+        cls.lock.acquire()
+        try:
+            cls.sessions[sid] = obj
+        finally:
+            cls.lock.release()
+
+
 class Handler(ThreadingMixIn, BaseHTTPRequestHandler):
     def error_404(self):
         self.send_response(404, 'Not Found')
@@ -55,14 +88,36 @@ class Handler(ThreadingMixIn, BaseHTTPRequestHandler):
         st = stat(path)
         if not S_ISREG(st.st_mode):
             return self.error_404()
+        content_length = st.st_size
+
+        last_modified = formatdate(st.st_mtime)
+
+        if extension == 'html':
+            last_modified = formatdate()
+            host, port = self.client_address
+            hash_items = [ path, str(host), str(port), last_modified]
+            hash_text = ':'.join(hash_items)
+            hash_data = hash_text.encode('utf-8')
+            sid = hashlib.sha512(hash_data).hexdigest()
+            SessionManager.set(sid, {'status':'NEW'})
+
+            with open(path, 'r') as f:
+                content = f.read()
+            content = re.sub(r'\$SID\b', sid, content)
+            content = content.encode('utf-8')
+            content_length = len(content)
 
         self.send_response(200, 'OK')
         self.send_header('Content-Type', content_type)
-        self.send_header('Content-Length', st.st_size)
-        self.send_header('Last-Modified', formatdate(st.st_mtime))
+        self.send_header('Content-Length', content_length)
+        self.send_header('Last-Modified', last_modified)
         self.end_headers()
-        with open(path, 'rb') as content:
-            copyfileobj(content, self.wfile)
+
+        if extension == 'html':
+            self.wfile.write(content)
+        else:
+            with open(path, 'rb') as content:
+                copyfileobj(content, self.wfile)
 
     def out_json(self, obj, qid = ''):
         if qid:
@@ -104,6 +159,7 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 
 
 Cfg.init()
+SessionManager.init()
 
 address = ('0.0.0.0', 8000)
 httpd = ThreadedHTTPServer(address, Handler)
