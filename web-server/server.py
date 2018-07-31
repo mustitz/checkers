63e532a9bb7921913a2ff08b47c4a0d199b625c3
Andrii Sevastianov <mustitz@gmail.com>
2018-07-31 10:50:01 +0300

    Add static content output to  Web Server.
    So user can play with himself.

--- web-server/server.py
+++ web-server/server.py
@@ -1,16 +1,57 @@
 #!/usr/bin/env python3
 
 
-from socketserver import ThreadingMixIn
+from email.utils import formatdate
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import stat
+from os.path import abspath
+from socketserver import ThreadingMixIn
+from shutil import copyfileobj
+from stat import S_ISREG
+
+
+class Cfg:
+    @classmethod
+    def init(cls):
+        cls.content_types = {
+            'html' : 'text/html; charset=utf-8',
+            'js'   : 'application/javascript',
+            'ico'  : 'image/x-icon',
+            'png'  : 'image/png',
+        }
+
+        cls.base_path = abspath('.')
+        if cls.base_path[-1] != '/':
+            cls.base_path += '/'
 
 
 class Handler(ThreadingMixIn, BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200, 'OK')
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
+    def error_404(self):
+        self.send_response(404, 'Not Found')
         self.end_headers()
-        self.wfile.write('Not implemented!\n'.encode('utf-8'))
+
+    def do_GET(self):
+        self.protocol_version = 'HTTP/1.1'
+        if self.path == '/':
+            self.path = '/index.html'
+        path = abspath(Cfg.base_path + '/' + self.path)
+        if not path.startswith(Cfg.base_path):
+            return self.error_404()
+
+        extension = self.path.split('.')[-1]
+        content_type = Cfg.content_types.get(extension, '')
+        if not content_type:
+            return self.error_404()
+
+        st = stat(path)
+        if not S_ISREG(st.st_mode):
+            return self.error_404()
+
+        self.send_response(200, 'OK')
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', st.st_size)
+        self.send_header('Last-Modified', formatdate(st.st_mtime))
+        self.end_headers()
         with open(path, 'rb') as content:
             copyfileobj(content, self.wfile)
 
@@ -18,6 +59,8 @@ class Handler(ThreadingMixIn, BaseHTTPRequestHandler):
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
+
+Cfg.init()
 
 address = ('0.0.0.0', 8000)
 httpd = ThreadedHTTPServer(address, Handler)
