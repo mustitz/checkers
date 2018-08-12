6a0c7d8c1b2771d0fe1519ed612797414ebafc32
Mustitz <mustitz@gmail.com>
2018-08-12 09:54:02 +0300

    Reading players.
    Now players is a dictionary where key is player name and value is a
    dictionary with path and assume_rank fields.

--- script/tournament.py
+++ script/tournament.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from glob import glob
+
 import configargparse
 import os
 import sys
@@ -46,4 +48,23 @@ def read_assume_rank():
 def get_assume_rank(name):
     return assume_rank.get(name, len(assume_rank))
 
-assume_rank = read_assume_rank()
+
+def read_players():
+    files = glob('players/*.player')
+    if len(files) < 2:
+        print('No players')
+        sys.exit(1)
+    names = map(os.path.basename, files)
+    names = map(lambda name: os.path.splitext(name)[0], names)
+    return dict(zip(names, map(lambda path: {'path':path}, files)))
+
+
+def load_assume_rank(players):
+    global assume_rank
+    assume_rank = read_assume_rank()
+    for key in players.keys():
+        players[key]['assume_rank'] = get_assume_rank(key)
+
+
+players = read_players()
+load_assume_rank(players)
